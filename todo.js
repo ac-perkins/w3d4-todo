8529c564f36ec1f25af08d7781f487b29b5f7faf
Alex Perkins <acperkins@gmail.com>
2016-03-27 13:05:43 -0400

    localstorage working.

--- todo.js
+++ todo.js
@@ -5,9 +5,7 @@
 
   $('form').on('submit', function ( event ) {
     var newTodo = $('.new-todo').val();
-    itemList.push({todo: newTodo, timeStamp: Date.now()});
-    // itemList.todo = newTodo;
-    // itemList.timeStamp = Date.now();
+    itemList.push( {todo: newTodo, timeStamp: Date.now()} );
     console.log(itemList);
     event.preventDefault();
     $('.new-todo').val('');
@@ -21,14 +19,29 @@
       .append( $('<button>').attr({ class:'delete'}).text('X'));
 
     var newListItem = $('<li>')
+      .css( 'background-color', '#fff' )
       .append(newArticle);
 
-  $('.items')
-    .append(newListItem);
+    $('.items')
+      .sortable()               // Allows for click and drag sorting of the <li>s
+      .append(newListItem);
+
+    var todos = $('.items').html();
+    localStorage.setItem('todos', todos);
+    var incItems = itemsLeft;
+    localStorage.setItem('incItems', itemsLeft);
   });
 
+  if(localStorage.getItem('todos')) {
+$('.items').html(localStorage.getItem('todos'));
+}
+if(localStorage.getItem('incItems')) {
+$('.incomplete-items').html(localStorage.getItem('incItems'));
+itemsLeft = (localStorage.getItem('incItems'));
+}
+
   // Toggles completed class on selected article
-  $( 'ul' ).on( 'click', '.check', function() {
+  $( 'ul' ).on( 'click', '.check', function(event) {
     $(this).closest('article').toggleClass('completed');
     if ($(this).closest('article').hasClass('completed')) {
       itemsLeft--;
@@ -37,70 +50,88 @@
       itemsLeft++;
       $('.incomplete-items').text(itemsLeft);
     }
+    var todos = $('.items').html();
+    localStorage.setItem('todos', todos);
+    var incItems = itemsLeft;
+    localStorage.setItem('incItems', itemsLeft);
   });
 
-  // var editTodo = '';
   // Allows for editing current list items
-  $( 'ul' ).on( 'click', 'p', function() {
+  $( 'ul' ).on( 'click', 'p', function(event) {
     $(this).closest('article').addClass('editing');
     $( ".edit-todo" ).focus();
 
-    $('.edit-todo').keydown( function ( event ) {    // Put EVENT IN THESE FUNCTIONS
+    $('.edit-todo').keydown( function (event) {    // Put EVENT IN THESE FUNCTIONS
       if (event.keyCode === 13) {
         var editTodo = $(this).closest('article').find('.edit-todo').val();
-        console.log(editTodo);
-        // console.log(newTodo);
         $(this).closest('article').find('p').text(editTodo);
         $(this).closest('article').removeClass('editing');
         event.preventDefault();
+        var todos = $('.items').html();
+        localStorage.setItem('todos', todos);
       }
       });
   });
 
   // Removes selected ToDO/li when X is clicked
-  $( 'ul' ).on( 'click', '.delete', function() {
+  $( 'ul' ).on( 'click', '.delete', function(event) {
     $(this).closest('li').remove();
-    itemsLeft--;
+
+    if (!$(this).closest('article').hasClass('completed')) {
+      itemsLeft--;
+      $('.incomplete-items').text(itemsLeft);
+    }
+    var incItems = itemsLeft;
+    localStorage.setItem('incItems', itemsLeft);
+
+    // itemsLeft--;
     $('.incomplete-items').text(itemsLeft);
 
-    itemList.forEach(function removeItem(time, i) {     // THIS NEEDS TO WORK!
-      if (itemList[i].timeStamp ===)
-        itemList.split(i);
-      console.log(itemList);
-    });
+    var todos = $('.items').html();
+    localStorage.setItem('todos', todos);
+    // itemList.forEach(function removeItem(time, i) {     // THIS NEEDS TO WORK!
+    //   if (itemList[i].timeStamp ===)
+    //     itemList.split(i);
+    //   console.log(itemList);
+    // });
   });
 
   // Shows ALL items when clicked
-  $('.show-all').on( 'click', function() {
-    $( 'article' ).closest('li').css( 'display', 'inherit' );
+  $('.show-all').on( 'click', function(event) {
+    $( 'article' ).closest('li').css( 'display', 'block' );
     $('.show-all').addClass('active');
     $('.show-active').removeClass('active');
     $('.show-completed').removeClass('active');
   });
 
   // Shows ACTIVE items when clicked
-  $('.show-active').on( 'click', function() {
+  $('.show-active').on( 'click', function(event) {
     $( '.completed' ).closest('li').css( 'display', 'none' );
-    $( 'article' ).not( '.completed' ).closest('li').css( 'display', 'inherit' );
+    $( 'article' ).not( '.completed' ).closest('li').css( 'display', 'block' );
     $('.show-active').addClass('active');
     $('.show-all').removeClass('active');
     $('.show-completed').removeClass('active');
   });
 
   // Shows COMPLETED items when clicked
-  $('.show-completed').on( 'click', function() {
+  $('.show-completed').on( 'click', function(event) {
     $( 'article' ).not( '.completed' ).closest('li').css( 'display', 'none' );
-    $( '.completed' ).closest('li').css( 'display', 'inherit' );
+    $( '.completed' ).closest('li').css( 'display', 'block' );
     $('.show-completed').addClass('active');
     $('.show-all').removeClass('active');
     $('.show-active').removeClass('active');
   });
 
   // Removes all completed items when 'Clear completed' is clicked
-  $('.clear').on( 'click', function() {
+  $('.clear').on( 'click', function(event) {
     $('.completed').closest('li').remove();
-    $( 'article' ).not( '.completed' ).closest('li').css( 'display', 'inherit' );
+    $( 'article' ).not( '.completed' ).closest('li').css( 'display', 'block' );
+    var todos = $('.items').html();
+    localStorage.setItem('todos', todos);
+    var incItems = itemsLeft;
+    localStorage.setItem('incItems', itemsLeft);
+    // window.localStorage.clear();
+    // location.reload();
   });
-
 
 })();
